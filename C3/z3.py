f1a0ec145de755cadd7de736c900e2c851256354
foobarNano <ikwiatkowski@gmail.com>
2023-04-11 09:11:48 +0200

    idk

--- C3/z3.py
+++ C3/z3.py
@@ -1,41 +1,10 @@
 from random import randint
 
-board = [0,0,0,0,0,0,0,0,0]
-#   0 - Empty
-#   1 - Player1
-#   2 - Player2 / CPU
-
 points = [0,0]
 #   [P1, P2/CPU]
 
 def clearscreen():
     print('\n\n\n\n\n\n\n\n')
-
-def printboard():
-    print(board[0], '|', board[1], '|', board[2])
-    print('--+---+--')
-    print(board[3], '|', board[4], '|', board[5])
-    print('--+---+--')
-    print(board[6], '|', board[7], '|', board[8])
-# 0 | 0 | 0
-# --+---+--
-# 0 | 0 | 0
-# --+---+--
-# 0 | 0 | 0
-
-def checkboard():
-    for i in range(0, 3):
-        wcon1 = (board[3*i] == board[3*i+1] == board[3*i+2] != 0)
-        wcon2 = (board[i] == board[i+3] == board[i+6] != 0)
-        if wcon1 or wcon2: return board[i]
-    wcon3 = (board[0] == board[4] == board[8] != 0) or (board[2] == board[4] == board[5] != 0)
-    if wcon3:       return board[4]
-    if 0  in board: return 0
-    return -1
-#  1 - P1 won
-#  2 - P2/CPU won
-#  0 - Game not over
-# -1 - Draw
 
 while (True):
     clearscreen()
@@ -99,5 +68,3 @@ while (True):
         
         if (result == 1):   points[0]+=1
         elif (result == 2): points[1]+=1
-    
-    board.clear()
