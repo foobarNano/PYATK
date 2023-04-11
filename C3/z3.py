d9a237c7596da2d86e19412c298bd72f11b46da1
foobarNano <ikwiatkowski@gmail.com>
2023-04-11 09:58:23 +0200

    idk

--- C3/z3.py
+++ C3/z3.py
@@ -3,8 +3,19 @@ from random import randint
 points = [0,0]
 #   [P1, P2/CPU]
 
+choices = {
+    1 : 'ROCK',
+    2 : 'PAPER',
+    3 : 'SCISSORS'
+}
+
 def clearscreen():
     print('\n\n\n\n\n\n\n\n')
+
+def evaluate(p1choice: int, p2choice: int) -> int:
+    if p1choice == p2choice: return 0
+    if p1choice == p2choice%3 + 1: return 1
+    return 2
 
 while (True):
     clearscreen()
@@ -18,53 +29,43 @@ while (True):
     p2name = input("Name for P2: ") if pvpmode else "CPU"
 
     for rnum in range(1, rcount+1):
-        result = 0
+        clearscreen()
+        print(p1name, ':\t', points[0], sep='')
+        print(p2name, ':\t', points[1], sep='')
+        print('ROUND ', rnum, '/', rcount, sep='')
 
-        while True:
+        p1choice = int(input('\n' + p1name + '\'s choice: '))
+        p2choice = int()
+
+        if pvpmode:
             clearscreen()
             print(p1name, ':\t', points[0], sep='')
             print(p2name, ':\t', points[1], sep='')
             print('ROUND ', rnum, '/', rcount, sep='')
-            printboard()
 
-            while True:
-                choice = int(input('\n' + p1name + '\'s choice: '))
-                if choice < 1 or choice > 9:
-                    print('\tInvalid choice.')
-                    continue
-                if board[choice] != 0:
-                    print('\tSpace taken.')
-                    continue
-                board[choice-1] = 1
-                break
-            result = checkboard()
-            if result != 0: break
+            p2choice = int(input('\n' + p2name + '\'s choice: '))
+        else:
+            p2choice = randint(1, 3)
 
-            if pvpmode:
-                clearscreen()
-                print(p1name, ':\t', points[0], sep='')
-                print(p2name, ':\t', points[1], sep='')
-                print('ROUND ', rnum, '/', rcount, sep='')
-                printboard()
+        clearscreen()
+        print(choices[p1choice], ' VS ', choices[p2choice], sep='')
+        result = evaluate(p1choice, p2choice)
+        if result == 1:
+            print(p1name, 'wins!')
+            points[0] += 1
+        elif result == 2:
+            print(p2name, 'wins!')
+            points[1] += 1
+        else:
+            print('Draw!')
 
-                while True:
-                    choice = int(input('\n' + p2name + '\'s choice: '))
-                    if choice < 1 or choice > 9:
-                        print('\tInvalid choice.')
-                        continue
-                    if board[choice] != 0:
-                        print('\tSpace taken.')
-                        continue
-                    board[choice-1] = 2
-                    break
-            else:
-                avaible = list()
-                for i in range(0, 9):
-                    if board[i] == 0: avaible.append(i)
-                choice = randint(0, len(avaible))
-                board[choice] = 2
-            result = checkboard()
-            if(result!= 0): break
-        
-        if (result == 1):   points[0]+=1
-        elif (result == 2): points[1]+=1
+        input('\n[PRESS ENTER TO CONTINUE]')
+
+    clearscreen()
+    print('Final score:', points[0], 'to', points[1])
+    
+    if points[0] > points[1]:   print('The winner is ', p1name, '!', sep='')
+    elif points[1] > points[0]: print('The winner is ', p2name, '!', sep='')
+    else:                       print('It\'s a draw!')
+
+    input('\n[PRESS ENTER TO CONTINUE]')
