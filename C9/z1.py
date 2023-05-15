6d81d343a05b102a689f2d6c891040366882d6cb
foobarNano <ikwiatkowski@gmail.com>
2023-05-16 01:10:42 +0200

    1 am codin'

--- C9/z1.py
+++ C9/z1.py
@@ -1,9 +1,11 @@
 class Element:
+
     def __init__(self, data=None, nextE=None):
         self.data = data
         self.nextE: Element = nextE
 
 class MyLinkedList:
+
     def __init__(self):
         self.head: Element = None
         self.tail: Element = None
@@ -12,11 +14,25 @@ class MyLinkedList:
     def __str__(self):
         result = '[' + __recursive(self.head) + ']'
 
-    def get(self, e: Element) -> Element:
-        pass
+    def get(self, e: Element):
+        temp: Element = self.head
+        while temp != e:
+            if temp == None: return
+            temp = temp.nextE
+        return temp
+        
 
     def delete(self, e: Element):
-        pass
+        if self.head == e:
+            self.head = self.head.nextE
+            return
+
+        temp: Element = self.head
+        while temp.nextE != e:
+            if temp.nextE == None: return
+            temp = temp.nextE
+        temp.nextE = temp.nextE.nextE
+        
 
     def append(self, e: Element, func = None):
         pass
